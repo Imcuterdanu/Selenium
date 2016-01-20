852ac38043ae9c36ff8c457d65b90c3440eed177
Imcuterdanu <justinmai1217@gmail.com>
2016-01-19 18:57:09 -0800

    ugh

---
+++ LinkedIn.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+
+print(' Input your LinkedIn email address')
+userEmail = input()
+
+print(' Input your LinkedIn email password')
+userPassword = input()
+
+browser = webdriver.Firefox()
+browser.get('https://www.linkedin.com/uas/login')
+
+logInElem = browser.find_element_by_id('session_key-login')
+logInElem.click()
+
+emailElem = browser.find_element_by_id('session_key-login')
+emailElem.send_keys(userEmail)
+emailElem.submit()
+
+passwordElem.click()
+passwordElem = browser.find_element_by_id('session_key-password')
+
+passwordElem = browser.find_element_by_id('session_key-password')
+passwordElem.send_keys(userPassword)
+passwordElem.submit()
