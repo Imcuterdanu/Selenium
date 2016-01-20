af46b4370143e5cd9e802c08e2f3893bc756a563
Imcuterdanu <justinmai1217@gmail.com>
2016-01-19 19:46:06 -0800

    this giving me a headache *_+

--- LinkedIn.py
+++ LinkedIn.py
@@ -9,14 +9,11 @@ userPassword = input()
 browser = webdriver.Firefox()
 browser.get('https://www.linkedin.com/uas/login')
 
-logInElem = browser.find_element_by_id('session_key-login')
-logInElem.click()
-
 emailElem = browser.find_element_by_id('session_key-login')
 emailElem.send_keys(userEmail)
 
 passwordElem = browser.find_element_by_id('session_key-password')
-passwordElem.click()
+passwordElem.send_keys(userPassword)
 
 passwordElem.send_keys(userPassword)
 emailElem.submit()
